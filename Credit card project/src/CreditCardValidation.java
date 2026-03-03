ccaf01c6b943c0869fe6849e234d454658131b69
Felicianna.DeSantos <Felicianna.DeSantos@MU-C1-06.mullen.pvt.k12.co.us>
2026-03-03 15:01:49 -0700

    created textfile class

--- Credit card project/src/CreditCardValidation.java
+++ Credit card project/src/CreditCardValidation.java
@@ -1,11 +1,13 @@
+import java.io.IOException;
 
 public class CreditCardValidation
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
-				// TODO Auto-generated method stub
-
+				
+         
+         ReadingTextFile.creditTextFile();
 			}
 
 	}
---
+++ Credit card project/src/ReadingTextFile.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadingTextFile
+	{
+      public static void creditTextFile() throws IOException
+      {
+    	  Scanner creditFile = new Scanner(new File("CCnumbers.txt"));
+    	  while(creditFile.hasNext()) {
+    		  long cardNumbers = creditFile.nextLong();
+    		  System.out.println(cardNumbers);
+    	  }
+      }
+	}
